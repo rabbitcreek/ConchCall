50564fd32acd51f1ff7a55e6a362307da851b793
Robert Werner <rabbitcreek@mac.com>
2019-03-18 17:41:47 -1000

    finished transfer

---
+++ ConchCall/SecondViewController.swift
@@ -0,0 +1,180 @@
+//
+//  SecondViewController.swift
+//  ConchCall
+//
+//  Created by RobertW. on 3/18/19.
+//  Copyright © 2019 RobertW. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+
+class secondViewController: UIViewController, ARSCNViewDelegate{
+        var audioPlayer: AVAudioPlayer!
+        let soundEffect = URL(fileURLWithPath: Bundle.main.path(forResource: "0001", ofType: "wav")!)
+        var mover: Double = 0
+
+    @IBOutlet weak var sceneView: ARSCNView!
+     let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        self.sceneView.autoenablesDefaultLighting = true
+        sceneView.session.run(configuration)
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/conch3.dae")!
+        let planeNode = scene.rootNode.childNode(withName: "baseNode", recursively: true)
+        planeNode?.scale = SCNVector3Make(0.05, 0.05, 0.05)
+        planeNode?.position = SCNVector3(0.5,0,-0.5)
+        var count = -0.5
+        _ = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true){ t in
+            count = count + self.mover
+            print(count)
+            planeNode?.position = SCNVector3(0.5,0,count)
+            if count <= -20 {
+                t.invalidate()
+            }
+        }
+        
+        self.sceneView.scene.rootNode.addChildNode(planeNode!)
+        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8)
+        let forever = SCNAction.repeatForever(action)
+        planeNode?.runAction(forever)
+        // Set the scene to the view
+        //sceneView.scene = scene
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        sceneView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configuration.environmentTexturing = .automatic
+        // Create a session configuration
+        // let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
+     
+     return node
+     }
+     */
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+    
+    func addLight() {
+        // 1
+        let directionalLight = SCNLight()
+        directionalLight.type = .directional
+        // 2
+        directionalLight.intensity = 0
+        // 3
+        directionalLight.castsShadow = true
+        directionalLight.shadowMode = .deferred
+        // 4
+        directionalLight.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1)
+        // 5
+        directionalLight.shadowSampleCount = 10
+        // 6
+        let directionalLightNode = SCNNode()
+        directionalLightNode.light = directionalLight
+        directionalLightNode.rotation = SCNVector4Make(1, 1, 0, -Float.pi / 3)
+        sceneView.scene.rootNode.addChildNode(directionalLightNode)
+    }
+    @objc func tapped(recognizer :UIGestureRecognizer) {
+        // Get exact position where touch happened on screen of iPhone (2D coordinate)
+        let touchPosition = recognizer.location(in: sceneView)
+        
+        // 2.
+        // Conduct a hit test based on a feature point that ARKit detected to find out what 3D point this 2D coordinate relates to
+        let hitTestResult = sceneView.hitTest(touchPosition, types: .featurePoint)
+        
+        // 3.
+        if !hitTestResult.isEmpty {
+            guard let hitResult = hitTestResult.first else {
+                return
+            }
+            print(hitResult.worldTransform.columns.3)
+            // setting up url for your soundtrack
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundEffect)
+            audioPlayer.play()
+            
+            
+            
+        } catch {
+            // couldn't load file :(
+        }
+        
+        
+        // add the light to the scene
+        
+        
+        
+        
+        
+        
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) { // Change `2.0` to the desired number of seconds.
+            // Code you want to be delayed
+            
+            
+            self.addLight()
+            self.mover = -0.2
+            
+            
+            
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 20.0) { // Change `2.0` to the desired number of seconds.
+            // Code you want to be delayed
+            
+            self.performSegue(withIdentifier: "Return", sender: self)
+        }
+    }
+    
+    
+    
+}
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi/180}
+}
+
+
--- ConchCall/ViewController.swift
+++ ConchCall/ViewController.swift
@@ -8,13 +8,142 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import Solar
+import CoreLocation
+import UserNotifications
+class ViewController: UIViewController, CLLocationManagerDelegate, UNUserNotificationCenterDelegate{
+    
+    
+    let locationManager = CLLocationManager()
+    var latitudeMap : Double = 0.0
+    var longMap: Double = 0.0
 
+
+    @IBOutlet weak var sunsetView: UIImageView!
+    
+
+    @IBOutlet weak var sunsetTimeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0{
+            locationManager.stopUpdatingLocation()
+            longMap = location.coordinate.longitude
+            latitudeMap = location.coordinate.latitude
+            
+            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 
-
+    @IBAction func activateSchedule(_ sender: Any) {
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        let myLocation = CLLocationCoordinate2D(latitude: latitudeMap , longitude: longMap)
+        //let someDate = Date()
+        let someDate =  Calendar.current.date(byAdding: .day, value: -1, to: Date())
+        //someDate = someDate?.addingTimeInterval(20.0 * 60.0)
+        let solar = Solar(for: someDate!, coordinate: myLocation)
+        //let dateAsString = solar?.sunrise
+        let sunset = solar?.sunset
+        //let dateComponents = Calendar.current.dateComponents([.timeZone, .year, .month, .day, .hour, .minute, .second], from: sunset!)
+        var dateComponents = Calendar.current.dateComponents([  .hour, .minute, .second], from: sunset!)
+        let date = Date()
+        let calendar = Calendar.current
+        
+        let hour = calendar.component(.hour, from: date)
+        let minutes = calendar.component(.minute, from: date)
+        if hour == dateComponents.hour! && minutes >= dateComponents.minute! {
+            (sender as! UIButton).setTitle("Try Tommorow", for: [])
+            sunsetTimeLabel.text = ""
+        } else if hour > dateComponents.hour! {
+            (sender as! UIButton).setTitle("Try Tommorow", for: [])
+            sunsetTimeLabel.text = ""
+        }
+        else{
+            (sender as! UIButton).setTitle("Invitation Sent", for: [])
+            
+            //dateComponents.hour = 14
+            //dateComponents.minute = 17
+            print(" Sunset:  \(dateComponents)")
+            sunsetTimeLabel.text = " \( (dateComponents.hour ?? 0) - 12) : " + String(format: "%02d", dateComponents.minute ?? 0)
+            let center = UNUserNotificationCenter.current()
+            center.delegate = self
+            let content = UNMutableNotificationContent()
+            content.title = "Conch Call"
+            content.body = "Its Sundown Time to Blow the Conch"
+            content.categoryIdentifier = "alarm"
+            content.userInfo = ["customData": "fizzbuzz"]
+            content.sound = UNNotificationSound.default
+            //content.sound = UNNotificationSound(named: "MySound.aiff")
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+            //let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            let request = UNNotificationRequest(identifier: "last call", content: content, trigger: trigger)
+            UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+            
+            //let uuidString = UUID().uuidString
+        }
+        
+        
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void)
+    {
+        completionHandler(.alert)
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // pull out the buried userInfo dictionary
+        print("one")
+        performSegue(withIdentifier: "goToScreenTwo", sender: self)
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let customData = userInfo["customData"] as? String {
+            print("Custom data received: \(customData)")
+            
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                // the user swiped to unlock
+                print("Default identifier")
+                performSegue(withIdentifier: "goToScreenTwo", sender: self)
+                
+                
+            case "show":
+                // the user tapped our "show more info…" button
+                performSegue(withIdentifier: "goToScreenTwo", sender: self)
+                print("Show more information…")
+                break
+                
+            default:
+                break
+            }
+        }
+        
+        // you must call the completion handler when you're done
+        completionHandler()
+    }
+    
+    
+    @IBAction func surpriseConch(_ sender: Any) {
+    }
 }
+
 
